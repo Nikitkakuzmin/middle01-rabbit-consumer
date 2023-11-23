e00d6c53125ff0b9125a445773956877a2f03dc1
Nikitkakuzmin <134796013+Nikitkakuzmin@users.noreply.github.com>
2023-11-23 13:27:50 +0600

    first commit

---
+++ src/test/java/kz/nik/rabbitconsumer/RabbitconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nik.rabbitconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
